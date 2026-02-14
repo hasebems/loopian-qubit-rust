df8fb551e353940e2244282dd0530321eae34688
HASEBE Masahiko <JCA03205@gmail.com>
2026-02-14 20:16:13 +0900

    レジスタに設定する値を追加

--- src/devices/at42qt.rs
+++ src/devices/at42qt.rs
@@ -1,31 +1,40 @@
 /// AT42QT1070 （ただし実体はMUXの向こうにあるので addr は固定で良い）
 pub struct At42Qt1070 {
-    addr: u8, // 固定I2Cアドレス
 }
 
 impl At42Qt1070 {
     const ADDR: u8 = 0x1B;
+    const _STATUS: u8 = 2;
+    const LP_MODE: u8 = 54;
+    const MAX_DUR: u8 = 55;
+
     pub const fn new() -> Self {
-        Self { addr: Self::ADDR }
+        Self {}
     }
 
     /// 初期化
-    pub async fn init<I2C>(&mut self, _i2c: &mut I2C) -> Result<(), I2C::Error>
+    pub async fn init<I2C>(&mut self, i2c: &mut I2C) -> Result<(), I2C::Error>
     where
         I2C: embedded_hal_async::i2c::I2c,
     {
-        // i2c.write(self.addr, &[reg, val]).await?;
-        // i2c.write(self.addr, &[...]).await?;
-        Ok(())
+        let i2cdata = [Self::LP_MODE, 0];
+        i2c.write(Self::ADDR, &i2cdata).await?;
+        let i2cdata = [Self::MAX_DUR, 0];
+        i2c.write(Self::ADDR, &i2cdata).await
     }
 
     /// 7キー分などの「現在状態ビット」を返す（例：bit0..bit6）
-    pub async fn read_state<I2C>(&mut self, i2c: &mut I2C) -> Result<u8, I2C::Error>
+    pub async fn read_state<I2C>(&mut self, i2c: &mut I2C, key: u8, reference: bool) -> Result<u16, I2C::Error>
     where
         I2C: embedded_hal_async::i2c::I2c,
     {
-        let mut buf = [0u8; 1];
-        i2c.write_read(self.addr, &[0], &mut buf).await?;
-        Ok(buf[0])
+        let mut buf = [0u8; 2];
+        let wr_adrs = if reference {
+            18 + key * 2
+        } else {
+            4 + key * 2  //  0-6
+        };  
+        i2c.write_read(Self::ADDR, &[wr_adrs], &mut buf).await?;
+        Ok(buf[0] as u16 * 256 + buf[1] as u16)
     }
 }
--- src/devices/pca9544.rs
+++ src/devices/pca9544.rs
@@ -1,18 +1,16 @@
 pub struct Pca9544 {
-    addr: u8,
 }
 
 impl Pca9544 {
     const ADDR: u8 = 0x74;
     pub const fn new() -> Self {
-        Self { addr: Self::ADDR }
+        Self {}
     }
 
-    pub async fn select<I2C>(&self, i2c: &mut I2C, ch: u8) -> Result<(), I2C::Error>
+    pub async fn select<I2C>(&self, i2c: &mut I2C, dev: u8, ch: u8) -> Result<(), I2C::Error>
     where
         I2C: embedded_hal_async::i2c::I2c,
     {
-        let ctrl = self.addr + ch;
-        i2c.write(self.addr, &[ctrl]).await
+        i2c.write(Self::ADDR + dev, &[0x04 + ch]).await
     }
 }
