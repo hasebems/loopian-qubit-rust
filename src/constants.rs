a8154ea895532b2f7a80161a95f0d7e77402c28a
HASEBE Masahiko <JCA03205@gmail.com>
2026-02-24 20:35:08 +0900

    AT42QTをバルク読み出しに変更

--- src/constants.rs
+++ src/constants.rs
@@ -19,9 +19,8 @@ pub const PCA9544_NUM_CHANNELS: u8 = 4; // PCA9544のチャネル数
 pub const PCA9544_NUM_DEVICES: u8 = 4; // PCA9544の台数
 pub const AT42QT_KEYS_PER_DEVICE: u8 = 6; // AT42QT1070
 
-pub const TOTAL_QT_KEYS: usize = (PCA9544_NUM_CHANNELS as usize)
-    * (PCA9544_NUM_DEVICES as usize)
-    * (AT42QT_KEYS_PER_DEVICE as usize);
+pub const TOTAL_CH: usize = (PCA9544_NUM_CHANNELS as usize) * (PCA9544_NUM_DEVICES as usize);
+pub const TOTAL_QT_KEYS: usize = TOTAL_CH * (AT42QT_KEYS_PER_DEVICE as usize);
 
 // MIDI Note Number
 pub const KEYBD_LO: u8 = 21; // A0
--- src/touch/read_touch.rs
+++ src/touch/read_touch.rs
@@ -9,6 +9,7 @@ use crate::{POINT0, POINT1, POINT2, POINT3};
 
 pub struct ReadTouch {
     refference: [u16; constants::TOTAL_QT_KEYS],
+    refference_counter: usize,
 }
 
 impl ReadTouch {
@@ -17,6 +18,7 @@ impl ReadTouch {
     pub fn new() -> Self {
         Self {
             refference: [0u16; constants::TOTAL_QT_KEYS],
+            refference_counter: 0,
         }
     }
 
@@ -44,21 +46,16 @@ impl ReadTouch {
         at42: &mut at42qt::At42Qt1070,
         i2c: &mut I2c<'static, I2C1, i2c::Async>,
     ) {
-        let mut data = TOUCH_RAW_DATA.lock().await;
+        let mut data = [0u16; constants::TOTAL_QT_KEYS];
         for ch in 0..constants::PCA9544_NUM_CHANNELS * constants::PCA9544_NUM_DEVICES {
             let dev = ch / constants::PCA9544_NUM_CHANNELS;
             let ch_in_dev = Self::CH_CONVERTION[(ch % constants::PCA9544_NUM_CHANNELS) as usize];
             pca.select(i2c, dev, ch_in_dev).await.ok();
-            for key in 0..constants::AT42QT_KEYS_PER_DEVICE {
-                if let Ok(raw_data) = at42.read_state(i2c, key, false).await {
+            if let Ok(raw_data) = at42.read_6key(i2c, false).await {
+                for key in 0..constants::AT42QT_KEYS_PER_DEVICE {
                     let sid = (ch * constants::AT42QT_KEYS_PER_DEVICE + key) as usize;
-
-                    if let Ok(ref_data) = at42.read_state(i2c, key, true).await {
-                        self.refference[sid] = ref_data;
-                    }
-
-                    if raw_data >= self.refference[sid] {
-                        data[sid] = raw_data - self.refference[sid];
+                    if raw_data[key as usize] >= self.refference[sid] {
+                        data[sid] = raw_data[key as usize] - self.refference[sid];
                     } else {
                         data[sid] = 0;
                     }
@@ -69,6 +66,32 @@ impl ReadTouch {
                 pca.disconnect(i2c, dev).await.ok();
             }
         }
+        {
+            // タッチセンサーの生データを Mutex で保護されたグローバル変数に保存
+            let mut raw_data = TOUCH_RAW_DATA.lock().await;
+            raw_data.copy_from_slice(&data);
+        }
+
+        if self.refference_counter == 0 {
+            for ch in 0..constants::PCA9544_NUM_CHANNELS * constants::PCA9544_NUM_DEVICES {
+                let dev = ch / constants::PCA9544_NUM_CHANNELS;
+                let ch_in_dev =
+                    Self::CH_CONVERTION[(ch % constants::PCA9544_NUM_CHANNELS) as usize];
+                pca.select(i2c, dev, ch_in_dev).await.ok();
+                if let Ok(raw_data) = at42.read_6key(i2c, true).await {
+                    for key in 0..constants::AT42QT_KEYS_PER_DEVICE {
+                        let sid = (ch * constants::AT42QT_KEYS_PER_DEVICE + key) as usize;
+                        self.refference[sid] = raw_data[key as usize];
+                    }
+                }
+                // PCA9544のチャネルが最後のときに切断する
+                if ch % constants::PCA9544_NUM_CHANNELS == constants::PCA9544_NUM_CHANNELS - 1 {
+                    pca.disconnect(i2c, dev).await.ok();
+                }
+            }
+        }
+        self.refference_counter = (self.refference_counter + 1) % 48;
+
         POINT0.store(data[0] as u8, Ordering::Relaxed);
         POINT1.store(data[1] as u8, Ordering::Relaxed);
         POINT2.store(data[2] as u8, Ordering::Relaxed);
