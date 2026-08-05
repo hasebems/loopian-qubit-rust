b88f6b0772847488e4ed4d9b92c8bbb98c15c27e
HASEBE Masahiko <JCA03205@gmail.com>
2026-08-05 20:40:41 +0900

    エラーの周期を変えて見やすくする

--- src/main.rs
+++ src/main.rs
@@ -509,9 +509,9 @@ async fn core1_led_task(mut led: Output<'static>) {
 
             for _ in 0..tens {
                 led.set_low();
-                Timer::after_millis(150).await;
+                Timer::after_millis(250).await;
                 led.set_high();
-                Timer::after_millis(150).await;
+                Timer::after_millis(250).await;
             }
 
             // 十の位と一の位の区切り
@@ -519,9 +519,9 @@ async fn core1_led_task(mut led: Output<'static>) {
 
             for _ in 0..ones {
                 led.set_low();
-                Timer::after_millis(150).await;
+                Timer::after_millis(250).await;
                 led.set_high();
-                Timer::after_millis(150).await;
+                Timer::after_millis(250).await;
             }
 
             // 次の表示シーケンスまで待機
